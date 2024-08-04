755f8f30785e3e6acb884c97732878be6d2483d3
Fyurij <fyurij.zingmail.com>
2024-08-04 12:37:19 +0300

    Add strategies

--- Snake_Game/Snake_Game.cpp
+++ Snake_Game/Snake_Game.cpp
@@ -12,6 +12,8 @@
 #include <thread>
 #include <chrono>
 #include <deque>
+#include <memory>
+#include <algorithm>
 
 
 
@@ -50,52 +52,169 @@ bool operator==(const Koordinates& lhs, const Koordinates& rhs)
 
 
 
+class Strategy
+{
+public:
+    virtual ~Strategy() = default;
+    virtual bool MoveUp(Koordinates& head) = 0;
+    virtual bool MoveDown(Koordinates& head) = 0;
+    virtual bool MoveLeft(Koordinates& head) = 0;
+    virtual bool MoveRight(Koordinates& head) = 0;
+};
+
+class StandartStrategy : public Strategy
+{
+private:
+    int height;
+    int width;
+public:
+    StandartStrategy(int height_, int width_)
+        :height(height_)
+        ,width(width_)
+    {}
+
+    bool MoveUp(Koordinates& head) override
+    {
+        --head.y;
+        if (head.y > -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool MoveDown(Koordinates& head) override
+    {
+        ++head.y;
+        if (head.y < height)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool MoveLeft(Koordinates& head) override
+    {
+        --head.x;
+        if (head.x > -1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool MoveRight(Koordinates& head) override
+    {
+        ++head.x;
+        if (head.x < width)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+class EndlessStrategy : public Strategy
+{
+private:
+    int height;
+    int width;
+public:
+    EndlessStrategy(int height_, int width_)
+        :height(height_)
+        , width(width_)
+    {}
+
+    bool MoveUp(Koordinates& head) override
+    {
+        --head.y;
+        if (head.y == -1)
+        {
+            head.y = height - 1;
+        }
+        return true;
+    }
+
+    bool MoveDown(Koordinates& head) override
+    {
+        ++head.y;
+        if (head.y == height)
+        {
+            head.y = 0;
+        }
+        return true;
+    }
+
+    bool MoveLeft(Koordinates& head) override
+    {
+        --head.x;
+        if (head.x == -1)
+        {
+            head.x = width - 1;
+        }
+        return true;
+    }
+
+    bool MoveRight(Koordinates& head) override
+    {
+        ++head.x;
+        if (head.x == width)
+        {
+            head.x = 0;
+        }
+        return true;
+    }
+};
+
+
+
 class Snake
 {
 private:
     std::deque<Koordinates> body;
-
+    std::unique_ptr<Strategy> strategy;
 private:
     void UpdateBody(const Koordinates& newHead, int foodPosX, int foodPosY)
     {
-        body.push_front(newHead);
-        if (!EatingFood(foodPosX, foodPosY))
+        if (!EatingFood(foodPosX, foodPosY, newHead))
         {
             body.pop_back();
         }
+        body.push_front(newHead);
     }
 
 public:
-    Snake()
+    Snake(std::unique_ptr<Strategy> strategy_)
         :body({ {1,1} })
+        ,strategy(std::move(strategy_))
     {}
 
-    bool EatingFood(int foodPosX, int foodPosY) const
+    bool EatingFood(int foodPosX, int foodPosY, const Koordinates& newHead) const
     {
-        if (body[0].x == foodPosX && body[0].y == foodPosY)
+        if (newHead.x == foodPosX && newHead.y == foodPosY)
         {
             return true;
         }
         return false;
     }
 
-    bool IsSnakeBody(const Koordinates& square) const
+    bool IsSnakeBody(const Koordinates& koordinates) const
     {
-        for (std::deque<Koordinates>::const_iterator i = body.cbegin(); i != body.cend(); ++i)
+        /*for (std::deque<Koordinates>::const_iterator i = body.cbegin(); i != body.cend() - 1; ++i)
         {
-            if (*i == square)
+            if (*i == koordinates)
             {
                 return true;
             }
         }
-        return false;
+        return false;*/
+        return std::any_of(body.cbegin(), body.cend() - 1, [&koordinates/*or just '&' for all arguments*/](const Koordinates& bodyPart) {return bodyPart == koordinates; });
     }
 
-    bool MoveUp(int foodPosX, int foodPosY, int width, int height)
+    bool MoveUp(int foodPosX, int foodPosY)
     {
         Koordinates newHead = body.front();
-        --newHead.y;
-        if (newHead.y > -1 && !IsSnakeBody(newHead))
+        if (strategy->MoveUp(newHead) && !IsSnakeBody(newHead))
         {
             UpdateBody(newHead, foodPosX, foodPosY);
             return true;
@@ -103,11 +222,10 @@ public:
         return false;
     }
 
-    bool MoveDown(int foodPosX, int foodPosY, int width, int height)
+    bool MoveDown(int foodPosX, int foodPosY)
     {
         Koordinates newHead = body.front();
-        ++newHead.y;
-        if (newHead.y < height && !IsSnakeBody(newHead))
+        if (strategy->MoveDown(newHead) && !IsSnakeBody(newHead))
         {
             UpdateBody(newHead, foodPosX, foodPosY);
             return true;
@@ -115,11 +233,10 @@ public:
         return false;
     }
 
-    bool MoveLeft(int foodPosX, int foodPosY, int width, int height)
+    bool MoveLeft(int foodPosX, int foodPosY)
     {
         Koordinates newHead = body.front();
-        --newHead.x;
-        if (newHead.x > -1 && !IsSnakeBody(newHead))
+        if (strategy->MoveLeft(newHead) && !IsSnakeBody(newHead))
         {
             UpdateBody(newHead, foodPosX, foodPosY);
             return true;
@@ -127,11 +244,10 @@ public:
         return false;
     }
 
-    bool MoveRight(int foodPosX, int foodPosY, int width, int height)
+    bool MoveRight(int foodPosX, int foodPosY)
     {
         Koordinates newHead = body.front();
-        ++newHead.x;
-        if (newHead.x < width && !IsSnakeBody(newHead))
+        if (strategy->MoveRight(newHead) && !IsSnakeBody(newHead))
         {
             UpdateBody(newHead, foodPosX, foodPosY);
             return true;
@@ -156,94 +272,25 @@ public:
 
 
 
-class Field
+class View
 {
 private:
-    //std::vector<std::vector<FieldStatus>> field;
-    int width;
-    int height;
-    Koordinates food;
-    Snake snake;
-public:
-    Field(int width_, int height_)
-        //:field(height_, std::vector<FieldStatus>(width_, FieldStatus::Empty))
-        :width(width_)
-        ,height(height_)
-        , food({1,1})
-    {
-        //field[SnakePosY][SnakePosX] = FieldStatus::Snake;
-    }
-
-    void GameOver(State& state)
+    void GameOver()
     {
         Term::cout << Term::clear_screen() << std::flush;
         Term::cout << Term::cursor_move(1, 1) << "GAME OVER" << std::endl << std::endl;
-        state = State::Quit;
+        Term::cout << "Press R to restart" << std::endl;
+        Term::cout << "Press Q to quit" << std::endl;
     }
 
-    void GenerateFood()
+    void RenderField(const std::vector<std::vector<FieldStatus>>& field)
     {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distribWidth(1, width - 1);
-        std::uniform_int_distribution<> distribHeight(1, height - 1);
-        while (snake.IsSnakeBody({ food.x, food.y }))
-        {
-            food.x = distribWidth(gen);
-            food.y = distribHeight(gen);
-        }
-    }
-
-    void Move(State& state, Moving& move)
-    {
-        bool WasMoving = false;
-        switch (move)
-        {
-        case Moving::Left:
-            WasMoving = snake.MoveLeft(food.x, food.y, width, height);
-            break;
-        case Moving::Right:
-            WasMoving = snake.MoveRight(food.x, food.y, width, height);
-            break;
-        case Moving::Up:
-            WasMoving = snake.MoveUp(food.x, food.y, width, height);
-            break;
-        case Moving::Down:
-            WasMoving = snake.MoveDown(food.x, food.y, width, height);
-            break;
-        default:
-            break;
-        }
-        if (WasMoving)
-        {
-            if (snake.EatingFood(food.x, food.y))
-            {
-                GenerateFood();
-            }
-        }
-        else
-        {
-            GameOver(state);
-        }
-    }
-
-    void Restart()
-    {
-        snake.Restart();
-        GenerateFood();
-    }
-
-    void PrintOut()
-    {
-        std::vector<std::vector<FieldStatus>> field (height, std::vector<FieldStatus>(width, FieldStatus::Empty));
-        snake.FillField(field);
-        field[food.y][food.x] = FieldStatus::Food;
         std::string print;
         bool IsFoodOnField = false;
         print.append(Term::cursor_move(1, 1));
-        for (int i = 0; i < height; ++i)
+        for (int i = 0; i < field.size(); ++i)
         {
-            for (int j = 0; j < width; ++j)
+            for (int j = 0; j < field[i].size(); ++j)
             {
                 if (field[i][j] == FieldStatus::Empty)
                 {
@@ -278,26 +325,130 @@ public:
             print.append(Term::color_bg(Term::Color::Name::Default) + "\n");
         }
         Term::cout << print << std::flush;
-        if (!IsFoodOnField)
-        {
-            GenerateFood();
-        }
         //Term::cout << Term::color_bg(Term::Color::Name::Default) << std::endl;
+    }
+public:
+    void Notify(const std::vector<std::vector<FieldStatus>>& field, State state)
+    {
+        if (state == State::Quit)
+        {
+            GameOver();
+        }
+        else
+        {
+            RenderField(field);
+        }
     }
 };
 
 
 
-int main()
+class Field
 {
-    try
+private:
+    //std::vector<std::vector<FieldStatus>> field;
+    int width;
+    int height;
+    Koordinates food;
+    Snake snake;
+    std::shared_ptr<View> view;
+public:
+    Field(int width_, int height_, const std::shared_ptr<View>& view_, std::unique_ptr<Strategy> strategy)
+        //:field(height_, std::vector<FieldStatus>(width_, FieldStatus::Empty))
+        :width(width_)
+        ,height(height_)
+        ,view(view_)
+        ,snake(std::move(strategy))
     {
-        Term::Screen screen = Term::screen_size();
-        //Field field(screen.columns() - 10, screen.rows() - 10);
-        Field field(20, 20);
-        Moving move = Moving::Empty;
-        State state = State::Game;
-        field.GenerateFood();
+        GenerateFood();
+        //field[SnakePosY][SnakePosX] = FieldStatus::Snake;
+    }
+
+    void GenerateFood()
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distribWidth(1, width - 1);
+        std::uniform_int_distribution<> distribHeight(1, height - 1);
+        food.x = distribWidth(gen);
+        food.y = distribHeight(gen);
+        while (snake.IsSnakeBody({ food.x, food.y }))
+        {
+            food.x = distribWidth(gen);
+            food.y = distribHeight(gen);
+        }
+    }
+
+    void Move(State& state, Moving& move)
+    {
+        bool WasMoving = false;
+        switch (move)
+        {
+        case Moving::Left:
+            WasMoving = snake.MoveLeft(food.x, food.y);
+            break;
+        case Moving::Right:
+            WasMoving = snake.MoveRight(food.x, food.y);
+            break;
+        case Moving::Up:
+            WasMoving = snake.MoveUp(food.x, food.y);
+            break;
+        case Moving::Down:
+            WasMoving = snake.MoveDown(food.x, food.y);
+            break;
+        default:
+            break;
+        }
+        std::vector<std::vector<FieldStatus>> field(height, std::vector<FieldStatus>(width, FieldStatus::Empty));
+        if (WasMoving)
+        {
+            if (snake.IsSnakeBody({ food.x, food.y }))
+            {
+                GenerateFood();
+            }
+            field[food.y][food.x] = FieldStatus::Food;
+            snake.FillField(field);
+        }
+        else
+        {
+            state = State::Quit;
+        }
+        view->Notify(field, state);
+    }
+
+    void Restart()
+    {
+        std::vector<std::vector<FieldStatus>> field(height, std::vector<FieldStatus>(width, FieldStatus::Empty));
+        snake.Restart();
+        snake.FillField(field);
+        GenerateFood();
+        field[food.y][food.x] = FieldStatus::Food;
+        view->Notify(field, State::Game);
+    }
+};
+
+
+
+class Controller
+{
+private:
+    int height = 10;
+    int width = 10;
+    Moving move = Moving::Empty;
+    State state = State::Game;
+    std::shared_ptr<View> view;
+public:
+    Controller(const std::shared_ptr<View>& view_)
+        :view(view_)
+    {}
+
+    void Run()
+    {
+        //Term::Screen screen = Term::screen_size();
+        //Field field(screen.columns() - 10, screen.rows() - 10);      
+        std::unique_ptr<Strategy> strategy = std::make_unique<EndlessStrategy>(height, width);
+        Field field(height, width, view, std::move(strategy));
+        field.Restart();
         while (state != State::Quit)
         {
             //field.PrintOut();
@@ -306,15 +457,16 @@ int main()
             if (!event.empty())
             {
                 Term::Key key(event);
+                //Term::cout << key.name() << std::endl;
                 if (key == Term::Key::q || key == Term::Key::Q)
                 {
-                    field.GameOver(state);
+                    state = State::Quit;
                 }
                 else if (key == Term::Key::r || key == Term::Key::R)
                 {
+                    Term::cout << Term::clear_screen() << std::flush;
                     field.Restart();
                     move = Moving::Empty;
-                    Term::cout << Term::clear_screen() << std::flush;
                 }
                 else if (key == Term::Key::ArrowDown)
                 {
@@ -344,28 +496,24 @@ int main()
                         move = Moving::Left;
                     }
                 }
-                //field.Move(state, move);
+                //field.Move(state, move)
             }
             if (move != Moving::Empty)
             {
                 field.Move(state, move);
             }
-            if (state == State::Game)
-            {
-                field.PrintOut();
-            }
             //Term::cout << Term::clear_screen() << std::flush;
             while (state == State::Quit)
             {
-                Term::cout << "Press R to restart" << std::endl;
-                Term::cout << "Press Q to quit" << std::endl;
+                std::vector<std::vector<FieldStatus>> temp;
+                view->Notify(temp, state);
                 Term::Event eventQuit = Term::read_event();
                 Term::Key keyQuit(eventQuit);
                 if (keyQuit == Term::Key::r || keyQuit == Term::Key::R)
                 {
+                    Term::cout << Term::clear_screen() << std::flush;
                     field.Restart();
                     move = Moving::Empty;
-                    Term::cout << Term::clear_screen() << std::flush;
                     state = State::Game;
                 }
                 else if (keyQuit == Term::Key::q || keyQuit == Term::Key::Q)
@@ -378,7 +526,18 @@ int main()
                 }
             }
         }
+    }
+};
 
+
+
+int main()
+{
+    try
+    {
+        std::shared_ptr<View> view;
+        Controller game(view);
+        game.Run();
     }
     catch (std::exception& ex)
     {
